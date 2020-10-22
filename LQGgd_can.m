643efa7235e69276f70467343ad668437e4d4989
Yang Zheng <zhengy093@gmail.com>
2020-10-22 14:44:06 +0100

    Gradient over canonical form

--- LQGgd_can.m
+++ LQGgd_can.m
@@ -1,9 +1,10 @@
-function [Kopt,Jopt,info] = LQGgd(A,B,C,Q,R,W,V,K0,opts)
+function [Kopt,Jopt,info] = LQGgd_can(A,B,C,Q,R,W,V,K0,opts)
 % Gradient descent algorithm for LQG problem
 %
 %     K_{t+1} = K_t - lambda * Grad(K_t)
-%  where we take a full gradient, and do not account for canonical form
-%        Step size is chosen via the Armijo Rule
+%  where we take a partial gradient in a canonical form
+%  Step size is chosen via the Armijo Rule
+%  Only works for SISO systems now
 %  
 %    Dynamics: A, B, C
 %    Weights:  Q, R
@@ -18,9 +19,9 @@ flag = 1; % continuous time systems
 % Setup default parameters  
 % ------------------------------------------------------------------------
 initsize = 1;
-alpha    = 0.01;     % backtrapping line search 
+alpha    = 1e-2;     % backtrapping line search 
 beta     = 0.5;
-tol      = 1e-6;    % tolerance of norm of gradient direction
+tol      = 1e-4;    % tolerance of norm of gradient direction
 MaxIter  = 1e4;      % maximum number of gradient steps
 Disp     = 50;
 
@@ -31,7 +32,6 @@ Disp     = 50;
 if(nargin > 8)
     initsize = opts.stepsize;
 end
-
 
 %
 Jcost   = zeros(MaxIter,0);
@@ -48,7 +48,22 @@ else
     J = trace(blkdiag(W,K.Bk*V*K.Bk')*Y);   % initial cost value
 end
 
-fprintf('Iter       ngradK      LQG cost   Step_size\n') 
+
+% ------------------------------------------------------------------------
+% Controllability canonical form  
+% ------------------------------------------------------------------------
+Q1  = ctrb(K.Ak,K.Bk);
+tmp = charpoly(K.Ak);
+Ak  = [zeros(n-1,1), eye(n-1);
+       -tmp(end:-1:2)];
+Bk  = zeros(n,1); Bk(end) = 1;
+Q2 = ctrb(Ak,Bk);
+T = Q2*Q1^(-1);
+Bk = T*K.Bk; Ck = K.Ck*T^(-1);
+
+K.Ak = Ak; K.Bk = Bk; K.Ck = Ck;
+
+fprintf('Iter       ngradK     part_grad     LQG cost   Step_size\n') 
 for Iter = 1:MaxIter 
     
     Jcost(Iter) = J;     % the LQG cost in the current step; 
@@ -57,11 +72,17 @@ for Iter = 1:MaxIter
     % --------------------------------------------------------------------
     [Grad_A,Grad_B,Grad_C,~,~] = LQGgrad(A,B,C,K,Q,R,W,V,flag);
     gradK = [zeros(m,p) Grad_C;
-            Grad_B,    Grad_A];  % put it into one matrix    
+            Grad_B,    Grad_A];  % put it into one matrix     
     ngradK = norm(gradK,'fro');
+   
+    % partial gradient
+    tmp = zeros(n,n); tmp(end,:) = Grad_A(end,:);
+    par_gradK = [zeros(m,p) Grad_C;
+                 zeros(n,p),tmp];
+    npar_gradK = norm(par_gradK,'fro');
 
     % stop the algorithm if the norm of gradient is small enough        
-    if ngradK < tol
+    if npar_gradK < tol
         break;
     end
     
@@ -69,8 +90,9 @@ for Iter = 1:MaxIter
     %    Update according to Armijo rule: 
     % -------------------------------------------------------------------     
     StepSize = initsize;
-    Kt.Ak    = K.Ak - StepSize*Grad_A;
-    Kt.Bk    = K.Bk - StepSize*Grad_B;
+    parA = zeros(n,n); parA(end,:) = Grad_A(end,:);
+    Kt.Ak    = K.Ak - StepSize*parA;
+    Kt.Bk    = K.Bk;% - StepSize*Grad_B;
     Kt.Ck    = K.Ck - StepSize*Grad_C;
     
     Acl      = [A B*Kt.Ck;Kt.Bk*C Kt.Ak];
@@ -80,14 +102,15 @@ for Iter = 1:MaxIter
 
     
     % Backtracking line search
-    while mEigAcl >= 0 || J - Jtemp < StepSize*alpha*trace(gradK'*gradK)
+    while mEigAcl >= 0 || J - Jtemp < StepSize*alpha*trace(par_gradK'*par_gradK)
         StepSize = beta*StepSize;
         if StepSize < 1.e-19
             warning('Gradient method gets stuck with very small step size!');
             break;
         end
-        Kt.Ak    = K.Ak - StepSize*Grad_A;
-        Kt.Bk    = K.Bk - StepSize*Grad_B;
+        parA = zeros(n,n); parA(end,:) = Grad_A(end,:);
+        Kt.Ak    = K.Ak - StepSize*parA;
+        Kt.Bk    = K.Bk;% - StepSize*Grad_B;
         Kt.Ck    = K.Ck - StepSize*Grad_C;
         Acl      = [A B*Kt.Ck;Kt.Bk*C Kt.Ak];
         mEigAcl  = max(real(eig(Acl)));
@@ -96,7 +119,7 @@ for Iter = 1:MaxIter
     end
 
     if mod(Iter,Disp) == 0 || Iter == 1
-        fprintf('%4d     %6.4E    %6.4E   %6.4E \n',Iter, ngradK, J, StepSize);
+        fprintf('%4d     %6.4E    %6.4E    %6.4E   %6.4E \n',Iter, ngradK, npar_gradK, J, StepSize);
     end
     
     % update the current step K
--- lqg_gd_ex1m.m
+++ lqg_gd_ex1m.m
@@ -33,18 +33,42 @@ Ck = -K;
 
 Ko.Ak = Ak; Ko.Bk = Bk;Ko.Ck = Ck;
 % cost
-hA = [A B*Ck;Bk*C Ak];
-Y  = lyap(hA',blkdiag(Qc,Ck'*R*Ck));
+hA    = [A B*Ck;Bk*C Ak];
+Y     = lyap(hA',blkdiag(Qc,Ck'*R*Ck));
 Jopt  = trace(blkdiag(W,Bk*Bk')*Y);
 
-%[K1,J1,info1] = LQGgd(A,B,C,Qc,R,W,V,Ko);
 % ---------------------------------------------
 % Gradient descent 
 % ---------------------------------------------
 K0.Ak = -eye(nx); K0.Bk = zeros(nx,ny);K0.Ck = rand(nu,nx);
-
 [K1,J1,info1] = LQGgd(A,B,C,Qc,R,W,V,K0);
-[J1,Jopt,(J1-Jopt)/Jopt]
+Hess1 = LQGhessfull(A,B,C,K1,Qc,R,W,V);  % hessian
+
+
+% another initial point
+K0.Ak = -eye(nx); K0.Bk = rand(nx,ny);K0.Ck = zeros(nu,nx);
+[K2,J2,info2] = LQGgd(A,B,C,Qc,R,W,V,K0);
+Hess2 = LQGhessfull(A,B,C,K2,Qc,R,W,V);  % hessian
+
+% another initial point
+K0.Ak = Ak; K0.Bk = Bk;K0.Ck = Ck+0.05*randn(nu,nx);
+
+[K3,J3,info3] = LQGgd(A,B,C,Qc,R,W,V,K0);
+Hess3 = LQGhessfull(A,B,C,K3,Qc,R,W,V);  % hessian
+
+[J1,J2,J3,Jopt]
+([J1,J2,J3]-Jopt)./Jopt
+
+G1 = ss(K1.Ak,K1.Bk,K1.Ck,[]);
+G2 = ss(K2.Ak,K2.Bk,K2.Ck,[]);
+G3 = ss(K3.Ak,K3.Bk,K3.Ck,[]);
+Go = ss(Ak,Bk,Ck,[]);
+
+[tf(G1),tf(G2),tf(G3),tf(Go)]
+
+% ------------------------------------------------------------------------
+%          Figure
+% ------------------------------------------------------------------------
 
 figure;
 index = 1:1:info1.iter;
@@ -54,13 +78,6 @@ xlabel('Iterations $t$','Interpreter','latex','FontSize',10);
 set(gcf,'Position',[250 150 400 300]);
 set(gca,'TickLabelInterpreter','latex')
 
-
-% another initial point
-K0.Ak = -eye(nx); K0.Bk = rand(nx,ny);K0.Ck = zeros(nu,nx);
-
-[K2,J2,info2] = LQGgd(A,B,C,Qc,R,W,V,K0);
-[J1,J2,Jopt,(J1-Jopt)/Jopt,(J2-Jopt)/Jopt]
-
 figure;
 index = 1:1:info2.iter;
 semilogy(index,info2.Jiter(index)-Jopt)
@@ -69,10 +86,6 @@ xlabel('Iterations $t$','Interpreter','latex','FontSize',10);
 set(gcf,'Position',[250 150 400 300]);
 set(gca,'TickLabelInterpreter','latex')
 
-% another initial point
-K0.Ak = Ak; K0.Bk = Bk;K0.Ck = Ck+0.05*randn(nu,nx);
-
-[K3,J3,info3] = LQGgd(A,B,C,Qc,R,W,V,K0);
 
 figure;
 index = 1:1:info3.iter;
@@ -84,74 +97,68 @@ set(gca,'TickLabelInterpreter','latex')
 
 
 
-G1 = ss(K1.Ak,K1.Bk,K1.Ck,[]);
-G2 = ss(K2.Ak,K2.Bk,K2.Ck,[]);
-G3 = ss(K3.Ak,K3.Bk,K3.Ck,[]);
 
-Go = ss(Ak,Bk,Ck,[]);
-
-[tf(G1),tf(G2),tf(G3),tf(Go)]
-
-
-% ------------------------------------------------------------------------
-%          Hessian
-% ------------------------------------------------------------------------
-[n,m] = size(B);
-[p,n] = size(C);
-
-Hess1 = zeros(n^2 + p*n + m*n);
-Indx = ones(m+n,p+n);
-Indx(1:m,1:p) = zeros(m,p);
-Indx = Indx(:);
-
-nonInd = find(Indx > 0);
-
-for i = 1:n^2 + p*n + m*n
-    for j = i:n^2 + p*n + m*n   
-        Delta1 = zeros((p+n)*(m+n),1);Delta1(nonInd(i)) = 1;
-        Delta1 = reshape(Delta1,m+n,p+n); 
-        Delta2 = zeros((p+n)*(m+n),1);Delta2(nonInd(j)) = 1;
-        Delta2 = reshape(Delta2,m+n,p+n);  
-        [Ja, Jb, Jc, H, info] = LQGhess(A,B,C,K1,Qc,R,W,V,Delta1,Delta2);
-        Hess1(i,j) = H;
-        Hess1(j,i) = H;
-    end
-end
-
-Hess2 = zeros(n^2 + p*n + m*n);
-Indx = ones(m+n,p+n);
-Indx(1:m,1:p) = zeros(m,p);
-Indx = Indx(:);
-
-nonInd = find(Indx > 0);
-
-for i = 1:n^2 + p*n + m*n
-    for j = i:n^2 + p*n + m*n   
-        Delta1 = zeros((p+n)*(m+n),1);Delta1(nonInd(i)) = 1;
-        Delta1 = reshape(Delta1,m+n,p+n); 
-        Delta2 = zeros((p+n)*(m+n),1);Delta2(nonInd(j)) = 1;
-        Delta2 = reshape(Delta2,m+n,p+n);  
-        [Ja, Jb, Jc, H, info] = LQGhess(A,B,C,K2,Qc,R,W,V,Delta1,Delta2);
-        Hess2(i,j) = H;
-        Hess2(j,i) = H;
-    end
-end
-
-Hess3 = zeros(n^2 + p*n + m*n);
-Indx = ones(m+n,p+n);
-Indx(1:m,1:p) = zeros(m,p);
-Indx = Indx(:);
-
-nonInd = find(Indx > 0);
-
-for i = 1:n^2 + p*n + m*n
-    for j = i:n^2 + p*n + m*n   
-        Delta1 = zeros((p+n)*(m+n),1);Delta1(nonInd(i)) = 1;
-        Delta1 = reshape(Delta1,m+n,p+n); 
-        Delta2 = zeros((p+n)*(m+n),1);Delta2(nonInd(j)) = 1;
-        Delta2 = reshape(Delta2,m+n,p+n);  
-        [Ja, Jb, Jc, H, info] = LQGhess(A,B,C,K3,Qc,R,W,V,Delta1,Delta2);
-        Hess3(i,j) = H;
-        Hess3(j,i) = H;
-    end
-end
+% 
+% 
+% % ------------------------------------------------------------------------
+% %          Hessian
+% % ------------------------------------------------------------------------
+% [n,m] = size(B);
+% [p,n] = size(C);
+% 
+% Hess1 = zeros(n^2 + p*n + m*n);
+% Indx = ones(m+n,p+n);
+% Indx(1:m,1:p) = zeros(m,p);
+% Indx = Indx(:);
+% 
+% nonInd = find(Indx > 0);
+% 
+% for i = 1:n^2 + p*n + m*n
+%     for j = i:n^2 + p*n + m*n   
+%         Delta1 = zeros((p+n)*(m+n),1);Delta1(nonInd(i)) = 1;
+%         Delta1 = reshape(Delta1,m+n,p+n); 
+%         Delta2 = zeros((p+n)*(m+n),1);Delta2(nonInd(j)) = 1;
+%         Delta2 = reshape(Delta2,m+n,p+n);  
+%         [Ja, Jb, Jc, H, info] = LQGhess(A,B,C,K1,Qc,R,W,V,Delta1,Delta2);
+%         Hess1(i,j) = H;
+%         Hess1(j,i) = H;
+%     end
+% end
+% 
+% Hess2 = zeros(n^2 + p*n + m*n);
+% Indx = ones(m+n,p+n);
+% Indx(1:m,1:p) = zeros(m,p);
+% Indx = Indx(:);
+% 
+% nonInd = find(Indx > 0);
+% 
+% for i = 1:n^2 + p*n + m*n
+%     for j = i:n^2 + p*n + m*n   
+%         Delta1 = zeros((p+n)*(m+n),1);Delta1(nonInd(i)) = 1;
+%         Delta1 = reshape(Delta1,m+n,p+n); 
+%         Delta2 = zeros((p+n)*(m+n),1);Delta2(nonInd(j)) = 1;
+%         Delta2 = reshape(Delta2,m+n,p+n);  
+%         [Ja, Jb, Jc, H, info] = LQGhess(A,B,C,K2,Qc,R,W,V,Delta1,Delta2);
+%         Hess2(i,j) = H;
+%         Hess2(j,i) = H;
+%     end
+% end
+% 
+% Hess3 = zeros(n^2 + p*n + m*n);
+% Indx = ones(m+n,p+n);
+% Indx(1:m,1:p) = zeros(m,p);
+% Indx = Indx(:);
+% 
+% nonInd = find(Indx > 0);
+% 
+% for i = 1:n^2 + p*n + m*n
+%     for j = i:n^2 + p*n + m*n   
+%         Delta1 = zeros((p+n)*(m+n),1);Delta1(nonInd(i)) = 1;
+%         Delta1 = reshape(Delta1,m+n,p+n); 
+%         Delta2 = zeros((p+n)*(m+n),1);Delta2(nonInd(j)) = 1;
+%         Delta2 = reshape(Delta2,m+n,p+n);  
+%         [Ja, Jb, Jc, H, info] = LQGhess(A,B,C,K3,Qc,R,W,V,Delta1,Delta2);
+%         Hess3(i,j) = H;
+%         Hess3(j,i) = H;
+%     end
+% end
